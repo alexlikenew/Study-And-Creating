85af79584e55dfadbecbffdca0e88a54e68786f0
Olek <alexlikenew@gmail.com>
2024-12-12 16:22:11 +0100

    Basic components created

--- pizza-menu/src/App.jsx
+++ pizza-menu/src/App.jsx
@@ -1,146 +1,69 @@
 import './App.css'
 
+const pizzaData = [
+	{
+		name: 'Focaccia',
+		ingredients: 'Bread with italian olive oil and rosemary',
+		price: 6,
+		photoName: 'pizzas/focaccia.jpg',
+		soldOut: false,
+	},
+	{
+		name: 'Pizza Margherita',
+		ingredients: 'Tomato and mozarella',
+		price: 10,
+		photoName: 'pizzas/margherita.jpg',
+		soldOut: false,
+	},
+	{
+		name: 'Pizza Spinaci',
+		ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+		price: 12,
+		photoName: 'pizzas/spinaci.jpg',
+		soldOut: false,
+	},
+	{
+		name: 'Pizza Funghi',
+		ingredients: 'Tomato, mozarella, mushrooms, and onion',
+		price: 12,
+		photoName: 'pizzas/funghi.jpg',
+		soldOut: false,
+	},
+	{
+		name: 'Pizza Salamino',
+		ingredients: 'Tomato, mozarella, and pepperoni',
+		price: 15,
+		photoName: 'pizzas/salamino.jpg',
+		soldOut: true,
+	},
+	{
+		name: 'Pizza Prosciutto',
+		ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+		price: 18,
+		photoName: 'pizzas/prosciutto.jpg',
+		soldOut: false,
+	},
+]
+
 function App() {
 	return (
-		<>
-			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-		</>
+		<div>
+			<h1>Hello React!</h1>
+			<Pizza />
+			<Pizza />
+			<Pizza />
+			<Pizza />
+		</div>
+	)
+}
+function Pizza() {
+	return (
+		<div>
+			<img src='../public/pizzas/spinaci.jpg' alt='spinacci' />
+			<h2>Pizza Spinaci</h2>
+			<p>Tomato, mozarella, spinach, and ricotta cheese</p>
+		</div>
 	)
 }
 
 export default App
-
-const data = [
-	{
-		id: 1,
-		title: 'The Lord of the Rings',
-		publicationDate: '1954-07-29',
-		author: 'J. R. R. Tolkien',
-		genres: ['fantasy', 'high-fantasy', 'adventure', 'fiction', 'novels', 'literature'],
-		hasMovieAdaptation: true,
-		pages: 1216,
-		translations: {
-			spanish: 'El señor de los anillos',
-			chinese: '魔戒',
-			french: 'Le Seigneur des anneaux',
-		},
-		reviews: {
-			goodreads: {
-				rating: 4.52,
-				ratingsCount: 630994,
-				reviewsCount: 13417,
-			},
-			librarything: {
-				rating: 4.53,
-				ratingsCount: 47166,
-				reviewsCount: 452,
-			},
-		},
-	},
-	{
-		id: 2,
-		title: 'The Cyberiad',
-		publicationDate: '1965-01-01',
-		author: 'Stanislaw Lem',
-		genres: ['science fiction', 'humor', 'speculative fiction', 'short stories', 'fantasy'],
-		hasMovieAdaptation: false,
-		pages: 295,
-		translations: {},
-		reviews: {
-			goodreads: {
-				rating: 4.16,
-				ratingsCount: 11663,
-				reviewsCount: 812,
-			},
-			librarything: {
-				rating: 4.13,
-				ratingsCount: 2434,
-				reviewsCount: 0,
-			},
-		},
-	},
-	{
-		id: 3,
-		title: 'Dune',
-		publicationDate: '1965-01-01',
-		author: 'Frank Herbert',
-		genres: ['science fiction', 'novel', 'adventure'],
-		hasMovieAdaptation: true,
-		pages: 658,
-		translations: {
-			spanish: '',
-		},
-		reviews: {
-			goodreads: {
-				rating: 4.25,
-				ratingsCount: 1142893,
-				reviewsCount: 49701,
-			},
-		},
-	},
-	{
-		id: 4,
-		title: "Harry Potter and the Philosopher's Stone",
-		publicationDate: '1997-06-26',
-		author: 'J. K. Rowling',
-		genres: ['fantasy', 'adventure'],
-		hasMovieAdaptation: true,
-		pages: 223,
-		translations: {
-			spanish: 'Harry Potter y la piedra filosofal',
-			korean: '해리 포터와 마법사의 돌',
-			bengali: 'হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন',
-			portuguese: 'Harry Potter e a Pedra Filosofal',
-		},
-		reviews: {
-			goodreads: {
-				rating: 4.47,
-				ratingsCount: 8910059,
-				reviewsCount: 140625,
-			},
-			librarything: {
-				rating: 4.29,
-				ratingsCount: 120941,
-				reviewsCount: 1960,
-			},
-		},
-	},
-	{
-		id: 5,
-		title: 'A Game of Thrones',
-		publicationDate: '1996-08-01',
-		author: 'George R. R. Martin',
-		genres: ['fantasy', 'high-fantasy', 'novel', 'fantasy fiction'],
-		hasMovieAdaptation: true,
-		pages: 835,
-		translations: {
-			korean: '왕좌의 게임',
-			polish: 'Gra o tron',
-			portuguese: 'A Guerra dos Tronos',
-			spanish: 'Juego de tronos',
-		},
-		reviews: {
-			goodreads: {
-				rating: 4.44,
-				ratingsCount: 2295233,
-				reviewsCount: 59058,
-			},
-			librarything: {
-				rating: 4.36,
-				ratingsCount: 38358,
-				reviewsCount: 1095,
-			},
-		},
-	},
-]
-
-function getBooks() {
-	return data
-}
-
-function getBook(id) {
-	return data.find(d => d.id === id)
-}
-
-let books = getBooks()
-books
