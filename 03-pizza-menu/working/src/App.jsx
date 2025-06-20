a572c690bce98d1160ed1b1cab1b5f4e643fa4bd
alexlikenew <alexlikenew@gmail.com>
2025-06-20 12:25:39 +0200

    Working directory changed to 'working'

---
+++ 03-pizza-menu/working/src/App.jsx
@@ -0,0 +1,72 @@
+import {useState} from 'react'
+
+import './App.css'
+
+const pizzaData = [
+    {
+        name: "Focaccia",
+        ingredients: "Bread with italian olive oil and rosemary",
+        price: 6,
+        photoName: "pizzas/focaccia.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Margherita",
+        ingredients: "Tomato and mozarella",
+        price: 10,
+        photoName: "pizzas/margherita.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Spinaci",
+        ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+        price: 12,
+        photoName: "pizzas/spinaci.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Funghi",
+        ingredients: "Tomato, mozarella, mushrooms, and onion",
+        price: 12,
+        photoName: "pizzas/funghi.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Salamino",
+        ingredients: "Tomato, mozarella, and pepperoni",
+        price: 15,
+        photoName: "pizzas/salamino.jpg",
+        soldOut: true,
+    },
+    {
+        name: "Pizza Prosciutto",
+        ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+        price: 18,
+        photoName: "pizzas/prosciutto.jpg",
+        soldOut: false,
+    },
+];
+console.log(pizzaData[1]);
+
+function App() {
+
+    return (
+        <div>
+            <h1>Hello react</h1>
+            <Pizza/>
+            <Pizza/>
+            <Pizza/>
+        </div>
+
+    )
+}
+
+function Pizza() {
+    return <div>
+        <img src="../public/pizzas/spinaci.jpg" alt="Pizza Spinachi"/>
+        <h2>Pizza</h2>
+        <p>Bread with italian olive oil and rosemary</p>
+    </div>
+}
+
+export default App
