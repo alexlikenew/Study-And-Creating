489e06d62e509c362504684578cdd4b7278e5602
alexlikenew <alexlikenew@gmail.com>
2025-06-20 17:45:06 +0200

    Challenge 4 completed succesfully

--- 04-steps/working/src/App.jsx
+++ 04-steps/working/src/App.jsx
@@ -2,33 +2,43 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+const messages = [
+  'Learn React ⚛️',
+  'Apply for jobs 💼',
+  'Invest your new income 🤑',
+];
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [step, setStep] = useState(1)
+    const [isOpen, setIsOpen] = useState(true)
+
+function handlePreviuos(){
+    step > 1 ? setStep(step-1) : null
+}function handleNext(){
+    step < 3 ?  setStep(step+1) : null
+}
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+          <button className='close' onClick={() => setIsOpen(!isOpen)}>&times;</button>
+          { isOpen && (<div className="steps">
+              <div className="numbers">
+                  <div className={`${step >= 1 ? 'active' : null}`}>1</div>
+                  <div className={`${step >= 2 ? 'active' : null}`}>2</div>
+                  <div className={`${step >= 3  ? 'active' : null}`}>3</div>
+              </div>
+
+              <p className="message">Step {step}: {messages[step-1]}</p>
+
+              <div className="buttons">
+                  <button style={{backgroundColor:"#7950f2" , color:"white"}} className="previous" onClick={handlePreviuos}>Previous</button>
+                  <button style={{backgroundColor:"#7950f2" , color:"white"}} className="next" onClick={handleNext}>Next</button>
+              </div>
+          </div>)
+
+          }
+      </>
+
   )
 }
 
