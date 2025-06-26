f90da1d8ae8bb812db6b393b02b1a9019b3b38f8
alexlikenew <alexlikenew@gmail.com>
2025-06-26 17:12:37 +0200

    Accordion challenge continued

---
+++ accordion/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    const faqs = [
+        {
+            title: "Where are these chairs assembled?",
+            text:
+                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+        },
+        {
+            title: "How long do I have to return my chair?",
+            text:
+                "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
+        },
+        {
+            title: "Do you ship to countries outside the EU?",
+            text:
+                "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
+        }
+    ];
+
+  return (
+    <>
+        <Accordion data={faqs} />
+    </>
+  )
+}
+
+function Accordion({data}){
+
+    return <div>
+        {data.map(( el, index) => <AccordionItem title={el.title} text={el.text} num={index} key={el.title} />)}
+    </div>
+}
+
+function AccordionItem({num ,title , text}){
+
+    const [isOpen , setIsOpen] = useState(false)
+
+    return <div className={`${isOpen && 'open item'} item`} onClick={()=>setIsOpen(!isOpen)}>
+        <p className='number'> {num < 9  ? '0' + ( num + 1 ) : (num + 1)}</p>
+        <p className='text'>{title}</p>
+        <p className='icon'>{isOpen ? '-' : '+'}</p>
+        {isOpen && <div className='content-box'>{text}</div>}
+    </div>
+}
+
+export default App
