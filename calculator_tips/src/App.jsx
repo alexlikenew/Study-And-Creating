6a3e31d6a6be332d0da69427b46a51e0def19ac2
alexlikenew <alexlikenew@gmail.com>
2025-07-02 12:30:31 +0200

    Calculator for tips completed

--- calculator_tips/src/App.jsx
+++ calculator_tips/src/App.jsx
@@ -6,12 +6,16 @@ import './App.css'
 function App() {
   const [summ, setSumm] = useState(0)
     const [tip , setTip] =useState(0)
-    const [frienTip , setFriendTip]  = useState(0)
-
+    const [friendTip , setFriendTip]  = useState(0)
+    const tipToPay = ( Number(tip) + Number(friendTip)) / 100 * Number(summ) ;
+  const summToPay = Number(summ) + tipToPay ;
   return (
     <>
-    <Price summ={summ} onSumm = {setSumm}>How much was the bill?</Price>
-    <Tip tip={tip} onTip ={setTip}>How did you like the service?</Tip>
+        <Price summ={summ} onSumm = {setSumm}>How much was the bill?</Price>
+        <Tip tip={tip} onTip ={setTip}>How did you like the service?</Tip>
+        <Tip tip={friendTip} onTip={setFriendTip}>How did your friend like the service?</Tip>
+        <Bill summ={summ} tipToPay ={tipToPay} summToPay={summToPay}>You pay :</Bill>
+        <Reset summ={summ} onSum={setSumm} onTip={setTip} onTipFriend={setFriendTip} />
     </>
   )
 }
@@ -29,16 +33,42 @@ function Price({summ , onSumm ,children}){
 
 function Tip({tip , onTip ,children}) {
 
+function handleTip(e) {
+    onTip(e.target.value)
+}
     return <div>
         <label htmlFor="tip">{children}</label>
-        <select name="" id="tip">
-            <option value="0">It was okay</option>
+        <select name="" id="tip" onChange={handleTip}>
+            <option value="0">It was bad</option>
             <option value="5">It was good</option>
             <option value="10">It was great</option>
             <option value="15">It was super</option>
             <option value="20">It was awesome</option>
         </select>
     </div>
+}
+
+function Bill({summ , tipToPay ,summToPay, children}) {
+
+    if (summ === 0) return
+
+        return <div>
+            {children} {summToPay} ( {summ} + {tipToPay} tip )
+        </div>
+}
+
+function Reset({summ ,onSum , onTip , onTipFriend}){
+
+    if (summ === 0 ) return
+
+    function resetCalculator(){
+        onSum(0);
+        onTip(0);
+        onTipFriend(0);
+    }
+return <div>
+    <button onClick={resetCalculator}>Reset</button>
+</div>
 }
 
 export default App
