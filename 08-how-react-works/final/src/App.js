179f8acb486de9ec80faee5b94fb1cb2902185dc
alexlikenew <alexlikenew@gmail.com>
2025-07-04 17:14:13 +0200

    Fixed problem with keys

--- 08-how-react-works/final/src/App.js
+++ 08-how-react-works/final/src/App.js
@@ -45,7 +45,7 @@ function Tabbed({ content }) {
       {activeTab <= 2 ? (
         <TabContent
           item={content.at(activeTab)}
-          key={content.at(activeTab).summary}
+        key={content.at(activeTab).summary}
         />
       ) : (
         <DifferentContent />
