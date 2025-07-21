c291612425783f8273dcd2da1da38847390a2090
alexlikenew <alexlikenew@gmail.com>
2025-07-21 06:53:41 +0200

    Added initial files for Application

---
+++ 11-worldwise/working/src/pages/AppLayout.jsx
@@ -0,0 +1,11 @@
+import {Link} from "react-router-dom";
+import PageNav from "../components/PageNav.jsx";
+import AppNav from "../components/AppNav.jsx";
+
+export default function AppLayout() {
+    return (
+        <div>
+            <AppNav/>
+        </div>
+    )
+}
