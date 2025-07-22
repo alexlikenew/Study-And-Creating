611806c3bf264062b84a498c49a1c2b930159ec4
alexlikenew <alexlikenew@gmail.com>
2025-07-22 07:05:49 +0200

    Added countries bloc to app

--- 11-worldwise/working/src/components/CitiList.jsx
+++ 11-worldwise/working/src/components/CitiList.jsx
@@ -1,10 +1,13 @@
 import styles from './CityList.module.css'
 import Spinner from "./Spinner.jsx";
 import CityItem from "./CityItem.jsx";
+import Message from "./Message.jsx";
 
 export default function CitiList({cities, isLoading}) {
 
     if (isLoading) return <Spinner/>
+
+    if (!cities.length) return <Message message={'Add your first city by clicking on a city on the Map'}/>
 
     return <ul className={styles.cityList}>
         {cities.map((city) => <CityItem city={city} key={city.id}/>)}
