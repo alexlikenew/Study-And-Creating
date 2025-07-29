b16db539bc0887d927e295ac5ceb023db377873b
Oleksandr Petryshyn <alexlikenew@gmail.com>
2025-07-29 06:08:02 +0200

    Created postcontext provider

--- 12-atomic-blog/starter/src/App-v1.js
+++ 12-atomic-blog/starter/src/App-v1.js
@@ -1,6 +1,5 @@
 import {createContext, useContext, useEffect, useState} from "react";
 import { faker } from "@faker-js/faker";
-import {PostProvider,PostContext} from "./PostContex";
 
 function createRandomPost() {
   return {
@@ -10,11 +9,33 @@ function createRandomPost() {
 }
 
 // 1) Create context
-
+const PostContext = createContext()
 
 function App() {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isFakeDark, setIsFakeDark] = useState(false);
 
-    const [isFakeDark, setIsFakeDark] = useState(false);
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
   // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
     function () {
@@ -24,26 +45,32 @@ function App() {
   );
 
   return (
-        <section>
-            <button
-                onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
-                className="btn-fake-dark-mode"
-            >
-                {isFakeDark ? "☀️" : "🌙"}
-            </button>
+      // 2)Provide a value to child components
+      <PostContext.Provider value={{
+        posts:searchedPosts,
+          onAddPost:handleAddPost,
+        onClearPosts:handleClearPosts,
+          searchQuery:searchQuery,
+          setSearchQuery:setSearchQuery,
+      }
 
-    <PostProvider>
-            <Header
+      }>
+    <section>
+      <button
+        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+        className="btn-fake-dark-mode"
+      >
+        {isFakeDark ? "☀️" : "🌙"}
+      </button>
 
-            />
-            <Main />
-            <Archive  />
-            <Footer />
-    </PostProvider>
-        </section>
+      <Header
 
-
-
+      />
+      <Main />
+      <Archive onAddPost={handleAddPost} />
+      <Footer />
+    </section>
+      </PostContext.Provider>
   );
 }
 
