df199b13aa6cda6596e4c0251771c2152fb0314d
Oleksandr Petryshyn <alexlikenew@gmail.com>
2025-08-10 09:57:19 +0200

    Added memoization to project and useeffects

--- 13-workout-timer/working/src/ToggleSounds.jsx
+++ 13-workout-timer/working/src/ToggleSounds.jsx
@@ -1,14 +1,12 @@
-import { memo } from "react";
-
 function ToggleSounds({ allowSound, setAllowSound }) {
   return (
-    <button
-      className="btn-sound"
-      onClick={() => setAllowSound((allow) => !allow)}
-    >
-      {allowSound ? "🔈" : "🔇"}
-    </button>
+      <button
+          className="btn-sound"
+          onClick={() => setAllowSound((allow) => !allow)}
+      >
+        {allowSound ? "🔈" : "🔇"}
+      </button>
   );
 }
 
-export default memo(ToggleSounds);
+export default ToggleSounds;
