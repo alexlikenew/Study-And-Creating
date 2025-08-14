2d7f65ec2a081d209e992534500583e73c2c31d4
Oleksandr Petryshyn <alexlikenew@gmail.com>
2025-08-14 12:35:33 +0200

    Added Tailwind styling

--- 16-fast-react-pizza/working/src/ui/Header.jsx
+++ 16-fast-react-pizza/working/src/ui/Header.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder.jsx";
 
 function Header() {
-    return (
-        <header>
-            <Link to='/'>Fast React</Link>
-            <SearchOrder/>
-        </header>
-    )
+  return (
+    <header className="bg-yellow-500">
+      <Link to="/">Fast React</Link>
+      <SearchOrder />
+    </header>
+  );
 }
 
 export default Header;
