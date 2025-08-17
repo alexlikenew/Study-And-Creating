567061f2db6b8ce45c83a157124a8cd5c081d5f5
Oleksandr Petryshyn <alexlikenew@gmail.com>
2025-08-17 11:40:03 +0200

    Added Supabase conenction to database

--- 17-the-wild-oasis/working/src/pages/Cabins.jsx
+++ 17-the-wild-oasis/working/src/pages/Cabins.jsx
@@ -1,13 +1,22 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import {useEffect} from "react";
+import {getCabins} from "../services/apiCabins.jsx";
 
 function Cabins() {
-  return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
-  );
+
+    useEffect(() => {
+        getCabins().then(data => console.log(data))
+    }, []);
+
+    return (
+        <Row type="horizontal">
+            <Heading as="h1">All cabins</Heading>
+            <p>TEST</p>
+            <img src="https://eaoovqrutckonnubrzyd.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
+                 alt=""/>
+        </Row>
+    );
 }
 
 export default Cabins;
---
+++ 17-the-wild-oasis/working/src/services/apiCabins.jsx
@@ -0,0 +1,16 @@
+import supabase from "./supabase.jsx";
+
+export async function getCabins() {
+
+    const {data, error} = await supabase
+        .from('cabins')
+        .select('*')
+
+    if (error) {
+        console.log(error)
+        throw new Error('Cabins cannot be loaded')
+    }
+
+    return data
+
+}
